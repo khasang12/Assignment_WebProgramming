4e416e69f3e3869b060fa9e7ca501468158e5c23
Thai Tran <1tranquocthai@gmail.com>
2022-11-19 10:38:14 +0700

    UPdate

--- client/src/App.js
+++ client/src/App.js
@@ -13,7 +13,7 @@ function App() {
       <Routes>
 
         <Route
-          path="/"
+          path="/admin/"
           element={
             <div>
               <AdminLayout setNavbarAdmin={setNavbarAdmin}/>
