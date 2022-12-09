2b2891999fbc5d523ea79cddb15e49b8d90dfaf1
Kha Sang <sang.khask12@hcmut.edu.vn>
2022-12-09 20:21:23 +0700

    fix login error

--- client/src/pages/user/AboutUs.js
+++ client/src/pages/user/AboutUs.js
@@ -32,7 +32,7 @@ const AboutUs = () => {
                         </h5>
                         <div class="accordion-collapse collapse" id="aboutAccordion-collapseOne" aria-labelledby="aboutAccordion-collapseOne" data-bs-parent="#aboutAccordion" >
                           <div class="accordion-body">
-                            <div class="row">
+                           <div class="row">
                               <div class="col-md-4"><img class="img-fluid" src={img} alt="..."/></div>
                               <div class="col-md-8">
                                 <p>One morning, when Gregor Samsa woke from troubled dreams, he found himself transformed in his bed into a horrible vermin. He lay on his armour-like back, and if he lifted his head a little he could see his brown belly, slightly domed and divided by arches into stiff sections.</p>
