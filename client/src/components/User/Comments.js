92a228f8b34729acf7fa47dbb8015196112ff0c5
vuong2404 <minhvuonglht@gmail.com>
2022-12-15 18:21:01 +0700

    finished comment

--- client/src/components/User/Comments.js
+++ client/src/components/User/Comments.js
@@ -1,55 +1,61 @@
 import { Pagination, Button } from 'react-bootstrap';
 
+import { icons } from 'react-icons/lib';
 import RateStar from './RateStar';
 import StarPercen from './StarPercen';
 
-const comments = [
-  {
-    avatar: 'https://cdn.icon-icons.com/icons2/1378/PNG/512/avatardefault_92824.png',
-    id: 'cmt1',
-    p_id: 'P1',
-    customer_id: 'C1',
-    customerName: 'Minh Vương',
-    content: 'Đắt vl',
-    update_at: '29/09/2022 02:22',
-    numStar: 2,
-    reply: [],
-  },
-  {
-    id: 'cmt2',
-    avatar: 'https://cdn.icon-icons.com/icons2/1378/PNG/512/avatardefault_92824.png',
-    p_id: 'P1',
-    customer_id: 'C2',
-    customerName: 'Thái',
-    content: 'Sản phẩm tạm được!',
-    update_at: '29/09/2022 02:22',
-    numStar: 4,
-    reply: [],
-  },
-  {
-    id: 'cmt3',
-    avatar: 'https://cdn.icon-icons.com/icons2/1378/PNG/512/avatardefault_92824.png',
-    p_id: 'P1',
-    customer_id: 'C3',
-    customerName: 'Hào',
-    content: 'Ngon bổ rẻ :>',
-    update_at: '29/09/2022 02:22',
-    numStar: 5,
-    reply: [],
-  },
-  {
-    id: 'cmt4',
-    avatar: 'https://cdn.icon-icons.com/icons2/1378/PNG/512/avatardefault_92824.png',
-    p_id: 'P1',
-    customer_id: 'C4',
-    customerName: 'Kha Sang',
-    content: 'Đỉnh của chóp ',
-    update_at: '29/09/2022 02:22',
-    numStar: 5,
-    reply: [],
-  },
-];
+import * as BiIcons from 'react-icons/bi';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import images from '../../assets/images';
+import { AiTwotoneStar } from 'react-icons/ai';
+
+function addComment() {
+  document.getElementById('test').style.display = 'block';
+  document.getElementById('addCmtButton').disabled = true;
+}
+
 function Comments({ product_id = '' }) {
+  const [comments, setComments] = useState([]);
+  const [numstar, setNumstar] = useState(3);
+
+  const getComments = async () => {
+    return await axios.get(`http://localhost:8080/api/comments/${product_id}`).then((res) => res.data);
+  };
+
+  const handleAddcomment = async (e) => {
+    e.preventDefault();
+    let form = e.target;
+    let comment = form.writecomment.value;
+    if (sessionStorage.getItem('user')) {
+      let user_id = JSON.parse(sessionStorage.getItem('user')).id;
+      if (numstar > 0) {
+        await axios({
+          method: 'post',
+          url: 'http://localhost:8080/api/comments/user',
+          data: {
+            product_id,
+            user_id,
+            comment,
+            num_rate: numstar,
+          },
+        });
+      }
+    } else {
+      alert('Vui lòng đăng nhập để đánh giá sản phẩm');
+    }
+
+    await getComments()
+      .then((res) => setComments(res))
+      .catch((err) => alert('err: ', err));
+
+    form.writecomment.value = '';
+  };
+
+  useEffect(() => {
+    getComments().then((res) => setComments(res));
+  }, [product_id]);
+
   return (
     <div id="comment">
       <h4 className="font-weight-normal mb-4">KHÁCH HÀNG NHẬN XÉT</h4>
@@ -60,44 +66,46 @@ function Comments({ product_id = '' }) {
           <p>({comments.length} đánh giá)</p>
         </div>
         {/* <div className="col-12">
-          <StarPercen />
-        </div> */}
+        <StarPercen />
+      </div> */}
         <div className="col-12 col-md-6 d-flex flex-column align-items-center">
           <p>Chia sẻ đánh giá của bạn</p>
-          <Button variant="danger">Viết đánh giá của bạn</Button>
+          <a href="#writecomment" className="btn btn-danger text-white">
+            Viết đánh giá của bạn
+          </a>
         </div>
       </div>
 
-      <div className="row border-bottom py-2">
+      <div className="d-flex align-items-center border-bottom py-2">
         <p className="ml-0 mb-2">Chọn xem đánh giá</p>
-        <div>
-          <select>
+
+        {/* <select>
             <option>Tất cả</option>
             <option>Có ảnh</option>
             <option>không có ảnh</option>
-          </select>
-          <select className="ml-3">
-            <option>Tất cả</option>
-            <option>1</option>
-            <option>2</option>
-            <option>3</option>
-            <option>4</option>
-            <option>5</option>
-          </select>
-        </div>
+          </select> */}
+        <select className="ml-3">
+          <option>Tất cả</option>
+          <option>1 sao</option>
+          <option>2 sao</option>
+          <option>3 sao</option>
+          <option>4 sao</option>
+          <option>5 sao</option>
+        </select>
       </div>
+
       {comments.map((item, index) => (
         <div key={index} className="border-bottom m-0 py-2">
           <div className="d-flex align-items-center">
-            <img width="30px" src={item.avatar} alt="" />
-            <span>{item.customerName}</span>
+            <img width="30px" src={item.avatar || images.defaultAvatar} alt="" />
+            <span>{item.first_name + item.last_name}</span>
+          </div>
+          <div className="">
+            <RateStar number={item.num_rate} />
+            <p>{item.comment}</p>
           </div>
           <div className="d-flex align-items-center">
-            <RateStar number={item.numStar} />
-            <span>{item.content}</span>
-          </div>
-          <div className="d-flex align-items-center">
-            <span className="ml-0 text-primary">Trả lời</span>
+            <span className="ml-0 text-primary">{item.status}</span>
             <span>{item.update_at}</span>
           </div>
         </div>
@@ -111,12 +119,35 @@ function Comments({ product_id = '' }) {
           <Pagination.Item>5</Pagination.Item>
         </Pagination>
       </div>
+      <div style={{ display: 'none' }} className="d-flex flex-column">
+        <div>
+          <AiTwotoneStar className={`${numstar > 0 && 'text-warning'}`} size={40} onClick={() => setNumstar(1)} />
+          <AiTwotoneStar className={`${numstar > 1 && 'text-warning'}`} size={40} onClick={() => setNumstar(2)} />
+          <AiTwotoneStar className={`${numstar > 2 && 'text-warning'}`} size={40} onClick={() => setNumstar(3)} />
+          <AiTwotoneStar className={`${numstar > 3 && 'text-warning'}`} size={40} onClick={() => setNumstar(4)} />
+          <AiTwotoneStar className={`${numstar > 4 && 'text-warning'}`} size={40} onClick={() => setNumstar(5)} />
+        </div>
+        <form name="commentForm" onSubmit={(e) => handleAddcomment(e)}>
+          <div class="form-group">
+            <label for="writecomment">
+              <span>
+                <BiIcons.BiComment></BiIcons.BiComment>
+              </span>{' '}
+              Viết bình luận của bạn ở đây nhé:{' '}
+            </label>
+            <textarea class="form-control" id="writecomment" name="writecomment" rows="5" cols="115"></textarea>
+          </div>
+          <div>
+            <button type="submit" class="btn btn-primary">
+              Đăng bình luận
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
-
 export default Comments;
-
 /**Note: Một số action sẽ bổ sung sau:
  * Thêm comment
  * Trả lời comment
--- server/routes/comment.route.php
+++ server/routes/comment.route.php
@@ -2,7 +2,12 @@
     include_once(dirname(__FILE__) . '/../controllers/comment.controller.php');
     include_once(dirname(__FILE__) . '/../middleware/auth.php');
 
-    $url = array_filter(explode('/', $_SERVER['REQUEST_URI']));
+    $url_components= parse_url($_SERVER['REQUEST_URI']);
+    $url = array_filter(explode('/', $url_components['path']));
+
+    $params = [];
+    if (count($url_components) > 1)
+         parse_str($url_components['query'], $params);
 
     $method = $_SERVER['REQUEST_METHOD'];
     session_start();
@@ -11,6 +16,15 @@
         if($url['3'] == 'all' and $method == 'GET'){
             try{
                 echo CommentController::getAllComment();
+                http_response_code(200);
+            }
+            catch(CustomError $e){
+                echo json_encode(['msg' => $e->getMessage()]);
+                http_response_code($e->getStatusCode());
+            }
+        } else if($method == 'GET') {
+            try{
+                echo CommentController::getProductComments($url[3]);
                 http_response_code(200);
             }
             catch(CustomError $e){
@@ -38,18 +52,29 @@
                 echo json_encode(['msg' => $e->getMessage()]);
                 http_response_code($e->getStatusCode());
             }
-        }
-        else{
+        } else if ($url[3] == 'user' && $method == 'POST') {
+            try{
+                $data = (array) json_decode(file_get_contents('php://input'));
+                echo CommentController::createUserComment($data);
+                http_response_code(200);
+
+            }
+            catch(CustomError $e){
+                echo json_encode(['msg' => $e->getMessage()]);
+                http_response_code($e->getStatusCode());
+            }
+        } 
+        else {
             
             http_response_code(404);
             echo json_encode(["msg" => 'Not found API!!!']);
         }
     }
     else{
-        if($method == 'POST'){
+        
+        if($method == 'POST') {
             try{
                 $data = (array) json_decode(file_get_contents('php://input'));
-                print_r($data);
                 echo CommentController::createComment($data);
                 http_response_code(200);
 
@@ -62,8 +87,7 @@
         else{
             http_response_code(404);
             echo json_encode(["msg" => 'Not found API!!!']);
-        }
-        
+        } 
     }
     session_destroy();
 ?>
